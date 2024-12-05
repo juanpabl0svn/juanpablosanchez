b611aea7a517376ec61408fafe789fe108ed779b
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2024-12-05 15:51:55 -0500

    added more information

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -23,8 +23,8 @@ export const ui = {
         "open-source": "Open Source",
         "credits": `© ${CURRENT_YEAR} Based in the design of <a href="https://midu.dev/" class="hover:underline font-bold" >midudev</a>. License <a href="https://github.com/midudev/porfolio.dev/tree/main" class="hover:underline font-bold">CC BY-NC 4.0.</a>`,
         "current": "Current",
-        "experience.spider-security": "Here en Spider Security",
-        "experience.dna-universe": "Here en DNA",
+        "experience.spider-security": "Contributed to the development and optimization of key modules for the company's primary service, 'Terque,' leveraging CodeIgniter 4 and PHP 8.3.2 to enhance platform efficiency and scalability. Maintained and modernized legacy code, ensuring application stability by upgrading outdated modules and migrating microservices to NESTJS and Spring Boot with Gradle. Improved database architecture and microservices structure, optimizing performance and system integrity through Azure DevOps pipelines. Delivered specialized technical support to address complex technical challenges.",
+        "experience.dna-universe": "Led the development of a comprehensive club platform for 'Sefty,' integrating Web 3.0 technologies such as cryptocurrencies, NFTs, and AI to enhance user experience and expand platform capabilities. Migrated to modern technologies like NEXT 14 and NESTJS, optimizing performance and scalability. Automated deployments with GitHub Actions and implemented NGINX as a reverse proxy and load balancer. Designed visually appealing interfaces aligned with 'Sefty’s' brand identity using TailwindCSS and Figma.",
         "exotik-zoo": "Exotik Zoo is a landing page that was made for a company that sells and provides exotic animal care services. The page was made with React and TailwindCSS.",
         "deep-empire-bros": "Deep Empire Bros is an association of lawyers and financiers who provide legal and financial advice services for all types of companies. The page was made with Next and TailwindCSS.",
         "about-me.description.1": `My name is Juan Pablo Sanchez, I am ${AGE} years old, I am a <strong>Systems Engineer</strong> from the University of Medellín, here in Colombia. Since I was a child I have been interested in the world of technology, mainly the functioning of the modern world: how the internet works, signals, electricity and how it is possible that a computer can do so many things through <strong>1 and 0</strong>.`,
@@ -32,6 +32,7 @@ export const ui = {
         "about-me.description.3": "In my day to day, I try to teach and give back some of what this incredible world has given me in my life, both to work colleagues, friends and family. I love sharing knowledge and learning from others. This world of technology fascinates me because we share everything and we have the same goal: <strong>learn, improve and teach</strong>.",
         "cv.spanish": "CV Spanish",
         "cv.english": "CV English",
+        "open-source.switch": "Switch toggle to change the theme of the page"
 
 
 
@@ -50,8 +51,8 @@ export const ui = {
         "open-source": "Código abierto",
         "credits": `© ${CURRENT_YEAR} Basado en el diseño de <a href="https://midu.dev/" class="hover:underline font-bold" >midudev</a>. Licencia <a href="https://github.com/midudev/porfolio.dev/tree/main" class="hover:underline font-bold">CC BY-NC 4.0.</a>`,
         "current": "Actual",
-        "experience.spider-security": "Aquí en Spider Security",
-        "experience.dna-universe": "Aquí en DNA",
+        "experience.spider-security": "Contribuí al desarrollo y optimización de módulos clave para el servicio principal de la empresa, 'Terque,' utilizando CodeIgniter 4 y PHP 8.3.2 para mejorar la eficiencia y escalabilidad de la plataforma. Mantuve y modernicé código heredado, garantizando la estabilidad de las aplicaciones al actualizar módulos obsoletos y migrar microservicios a NESTJS y Spring Boot con Gradle. Realicé mejoras en la arquitectura de bases de datos y la estructura de microservicios, optimizando el rendimiento y la integridad del sistema a través de pipelines de Azure DevOps. Brindé soporte técnico especializado para resolver desafíos complejos.",
+        "experience.dna-universe": "Lideré el desarrollo de una plataforma integral de clubes para 'Sefty,' integrando tecnologías Web 3.0 como criptomonedas, NFTs e IA para mejorar la experiencia del usuario y ampliar las capacidades de la plataforma. Migré a tecnologías modernas como NEXT 14 y NESTJS, optimizando el rendimiento y la escalabilidad. Automatizé los despliegues con GitHub Actions e implementé NGINX como proxy inverso y balanceador de carga. Diseñé interfaces visualmente atractivas alineadas con la identidad de marca de 'Sefty' utilizando TailwindCSS y Figma.",
         "exotik-zoo": "Exotik Zoo es una landing page que se realizó para una empresa la cual vende y propociona servicios de cuidado de animales exóticos. La página fue realizada con React y TailwindCSS.",
         "deep-empire-bros": "Deep Empire Bros es una asociación de abogados y financieros los cuales proporcionan servicios de asesoría legal y financiera para todo tipo de empresa. La página fue realizada con Next y TailwindCSS.",
         "about-me.description.1": `Me llamo Juan Pablo Sanchez, tengo ${AGE} años, soy <strong>Ingeniero de Sistemas</strong> de la Universidad de Medellín, aquí en Colombia. Desde pequeño me ha interesado el mundo de la tecnología, principalmente el funcionamiento del mundo moderno: cómo funciona el internet, las señales, la electricidad y cómo es posible que un computador pueda hacer tantas cosas mediante <strong>1 y 0</strong>.`,
@@ -59,6 +60,8 @@ export const ui = {
         "about-me.description.3": "En mi día a día, trato de enseñar y devolver algo de lo que este increíble mundo me ha aportado en mi vida, tanto a compañeros de trabajo, amigos y familiares. Me encanta compartir conocimiento y aprender de los demás. Este mundo de la tecnología me fascina porque compartimos todo y tenemos un mismo objetivo: <strong>aprender, mejorar y enseñar</strong>.",
         "cv.spanish": "CV Español",
         "cv.english": "CV Inglés",
+        "open-source.switch": "Switch toggle para cambiar el tema de la página"
+
 
     },
 } as const;
